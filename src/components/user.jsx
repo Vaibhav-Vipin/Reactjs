f487aa13c198343228097ca5310f19f943e9e7de
Vaibhav-Vipin <vbhvpn@gmail.com>
2021-07-09 06:45:54 +0530

    26: Class Components, States,props

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,11 +1,22 @@
-const User = (props) => {
+import React from "react";
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    //   create state
+    this.state = {
+      planet: "earth",
+    };
+  }
+  render() {
+    //   Logic and shows VIEW of webpage.
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <h3>{props.description}</h3>
-        </div>
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.description}</p>
+        <h4>{this.state.planet}</h4>
+      </div>
     );
+  }
 }
-
 export default User;
-// props =>proprties/paramters 
+//state
